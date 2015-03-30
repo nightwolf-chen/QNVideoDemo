bdd18c97fa1a2aecfc8ae16189b3365c121f8360
Jidong <466202783@qq.com>
2015-03-30 22:58:00 +0800

    集成七牛

---
+++ QNVideoDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QNVideoDemo
+//
+//  Created by ChenJidong on 15/3/30.
+//  Copyright (c) 2015年 nirvawolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
